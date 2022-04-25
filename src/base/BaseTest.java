94e30c45e7f04ab276d5219f0b509cf66339f08b
Chintan170218 <chintantiwari7@gmail.com>
2022-04-25 21:47:37 +0100

    com-nopcommerce-sw-3

---
+++ src/base/BaseTest.java
@@ -0,0 +1,30 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+
+    public static WebDriver driver;
+
+
+    public void openBrowser(String baseUrl){
+
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        // Launch the URL.
+        driver.get(baseUrl);
+        // Maximise Window
+        driver.manage().window().maximize();
+        // We give implicit time to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
